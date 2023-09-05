ac23ab9415c3d70c05ff4a63fa79e803d069ca26
Shaun Ferris <slb.ferris46@gmail.com>
2023-09-05 13:29:20 +0100

    Replace manifest boilerplate with actual name and descriptions and refactor the placeholder function in the background file

--- background.js
+++ background.js
@@ -1,27 +1,31 @@
 chrome.commands.onCommand.addListener((command) => {
-  console.log("Command fired");
-  if (command === "execute_browser_action") {
+  if (command === "convert_selection") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
       chrome.scripting.executeScript({
         target: { tabId: activeTab.id },
-        function: () => {
-          const selectedText = window.getSelection().toString();
-          if (selectedText) {
-            const convertedText = selectedText.toUpperCase();
-            navigator.clipboard
-              .writeText(convertedText)
-              .then(() => {
-                console.log("Text copied to clipboard:", convertedText);
-              })
-              .catch((error) => {
-                console.error("Error copying text to clipboard:", error);
-              });
-          } else {
-            console.log("No text selected.");
-          }
-        }
+        function: convertAndCopy
       });
     });
   }
 });
+
+function convertAndCopy() {
+  const selection = window.getSelection().toString();
+  if (selection) {
+    const converted = selection.toUpperCase();
+    navigator.clipboard
+      .writeText(converted)
+      .then(() => {
+        console.log("Spongecase converter: Selection copied!", converted);
+      })
+      .catch((error) => {
+        console.log(
+          "Spongecase converter: Error converting selection: ",
+          error
+        );
+      });
+  } else {
+    console.log("Spongecase converter: No text selected.");
+  }
+}
