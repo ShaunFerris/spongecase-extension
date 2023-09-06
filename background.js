c0764fe674545960ea776457152c8baad04100b7
Shaun Ferris <slb.ferris46@gmail.com>
2023-09-06 13:54:01 +0100

    Saving progress working out the notifcations piping problem. NOT currently working correctly

--- background.js
+++ background.js
@@ -1,27 +1,3 @@
-const successNotif = {
-  title: "Successful spOnGEcAsE conversion!",
-  type: "basic",
-  message:
-    "The text you highlighted has been converted and copied to the clipboard, go ahead and paste it somewhere",
-  iconUrl: "icon.png"
-};
-
-const failNotif = {
-  title: "Failed spOnGEcAsE conversion.",
-  type: "basic",
-  message:
-    "There was an error converting selected text, try reloading the extension.",
-  iconUrl: "icon.png"
-};
-
-const noSelectNotif = {
-  title: "No text selected",
-  type: "basic",
-  message:
-    "You pushed the hotkey for the spongecase extension but had no text selected to convert",
-  iconUrl: "icon.png"
-};
-
 chrome.commands.onCommand.addListener((command) => {
   if (command === "convert_selection") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -29,21 +5,30 @@ chrome.commands.onCommand.addListener((command) => {
       chrome.scripting
         .executeScript({
           target: { tabId: activeTab.id },
-          function: convertAndCopy
+          func: convertAndCopy
         })
-        .then(() => {
-          chrome.notifications.create(successNotif);
+        .then((results) => {
+          chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            func: logMsg,
+            args: results
+          });
         })
         .catch((error) => {
           if (error.message === "No text selected") {
-            chrome.notifications.create(failNotif);
+            chrome.notifications.create();
           } else {
-            chrome.notifications.create(noSelectNotif);
+            chrome.notifications.create();
           }
         });
     });
   }
 });
+
+function logMsg(msg) {
+  console.log(msg);
+  return msg;
+}
 
 function convertAndCopy() {
   function spongeCase(text) {
@@ -55,6 +40,30 @@ function convertAndCopy() {
       .join("");
   }
 
+  const responseNotifications = {
+    success: {
+      title: "Successful spOnGEcAsE conversion!",
+      type: "basic",
+      message:
+        "The text you highlighted has been converted and copied to the clipboard, go ahead and paste it somewhere",
+      iconUrl: "icon.png"
+    },
+    fail: {
+      title: "Failed spOnGEcAsE conversion.",
+      type: "basic",
+      message:
+        "There was an error converting selected text, try reloading the extension, or check the developer console for more information.",
+      iconUrl: "icon.png"
+    },
+    noInput: {
+      title: "No text selected",
+      type: "basic",
+      message:
+        "You pushed the hotkey for the spongecase extension but had no text selected to convert",
+      iconUrl: "icon.png"
+    }
+  };
+
   const selection = window.getSelection().toString();
   if (selection) {
     const converted = spongeCase(selection);
@@ -62,16 +71,17 @@ function convertAndCopy() {
       .writeText(converted)
       .then(() => {
         console.log("Spongecase converter: Selection copied!", converted);
+        return responseNotifications.success;
       })
       .catch((error) => {
         console.log(
           "Spongecase converter: Error converting selection: ",
           error
         );
-        throw error;
+        return responseNotifications.fail;
       });
   } else {
     console.log("Spongecase converter: No text selected");
-    throw new Error("No text selected");
+    return responseNotifications.noInput;
   }
 }
