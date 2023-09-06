20d467ddfdf435867dad617620f64f9c26cdcccf
Shaun Ferris <slb.ferris46@gmail.com>
2023-09-06 10:47:35 +0100

    add options objects for other errors, try a different approach for error propagation

--- background.js
+++ background.js
@@ -6,19 +6,41 @@ const successNotif = {
   iconUrl: "icon.png"
 };
 
+const failNotif = {
+  title: "Failed spOnGEcAsE conversion.",
+  type: "basic",
+  message:
+    "There was an error converting selected text, try reloading the extension.",
+  iconUrl: "icon.png"
+};
+
+const noSelectNotif = {
+  title: "No text selected",
+  type: "basic",
+  message:
+    "You pushed the hotkey for the spongecase extension but had no text selected to convert",
+  iconUrl: "icon.png"
+};
+
 chrome.commands.onCommand.addListener((command) => {
   if (command === "convert_selection") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
-      try {
-        chrome.scripting.executeScript({
+      chrome.scripting
+        .executeScript({
           target: { tabId: activeTab.id },
           function: convertAndCopy
+        })
+        .then(() => {
+          chrome.notifications.create(successNotif);
+        })
+        .catch((error) => {
+          if (error.message === "No text selected") {
+            chrome.notifications.create(failNotif);
+          } else {
+            chrome.notifications.create(noSelectNotif);
+          }
         });
-        chrome.notifications.create(successNotif);
-      } catch (error) {
-        chrome.notifications.create();
-      }
     });
   }
 });
@@ -49,6 +71,7 @@ function convertAndCopy() {
         throw error;
       });
   } else {
-    console.log("Spongecase converter: No text selected.");
+    console.log("Spongecase converter: No text selected");
+    throw new Error("No text selected");
   }
 }
